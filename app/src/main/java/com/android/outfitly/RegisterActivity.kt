a6a1a639274254a06fc9624fa44ad92b6025689f
Faundrabima <faundrabm13@gmail.com>
2024-11-28 15:53:28 +0700

    Create Register Page

---
+++ app/src/main/java/com/android/outfitly/RegisterActivity.kt
@@ -0,0 +1,9 @@
+package com.android.outfitly
+
+import androidx.appcompat.app.AppCompatActivity
+import by.kirich1409.viewbindingdelegate.viewBinding
+import com.android.outfitly.databinding.ActivityRegisterBinding
+
+class RegisterActivity : AppCompatActivity(R.layout.activity_register)  {
+    private val binding by viewBinding(ActivityRegisterBinding::bind)
+}
