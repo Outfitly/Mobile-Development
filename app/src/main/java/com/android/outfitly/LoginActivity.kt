01f92a87394e298d04b7f54762012673f5db3ec0
Faundrabima <faundrabm13@gmail.com>
2024-11-29 21:53:54 +0700

    Link Register page to Login

--- app/src/main/java/com/android/outfitly/LoginActivity.kt
+++ app/src/main/java/com/android/outfitly/LoginActivity.kt
@@ -1,13 +1,22 @@
 package com.android.outfitly
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import by.kirich1409.viewbindingdelegate.viewBinding
+import com.android.outfitly.databinding.ActivityLoginBinding
 
 class LoginActivity: AppCompatActivity(R.layout.activity_login) {
 
+
+    private val binding by viewBinding(ActivityLoginBinding::bind)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        // LoginActivity tidak perlu logika untuk sementara
-    }
 
+        binding.btnLogin.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
 }
