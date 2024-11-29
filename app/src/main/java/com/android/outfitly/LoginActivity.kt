58cff50e9fa515b544bd1f79ad084e7776d2ad89
Faundrabima <faundrabm13@gmail.com>
2024-11-29 20:16:45 +0700

    Added Login Page

---
+++ app/src/main/java/com/android/outfitly/LoginActivity.kt
@@ -0,0 +1,13 @@
+package com.android.outfitly
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class LoginActivity: AppCompatActivity(R.layout.activity_login) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // LoginActivity tidak perlu logika untuk sementara
+    }
+
+}
--- app/src/main/java/com/android/outfitly/RegisterActivity.kt
+++ app/src/main/java/com/android/outfitly/RegisterActivity.kt
@@ -1,5 +1,8 @@
 package com.android.outfitly
 
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.android.outfitly.databinding.ActivityRegisterBinding
@@ -7,4 +10,12 @@ import com.android.outfitly.databinding.ActivityRegisterBinding
 class RegisterActivity : AppCompatActivity(R.layout.activity_register)  {
     private val binding by viewBinding(ActivityRegisterBinding::bind)
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding.btnRegister.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+    }
 }
